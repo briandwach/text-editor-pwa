03db89c61823bfc6b9f67f3c63c073900845251e
livefromthelevel <brian.d.wach@gmail.com>
2024-04-28 16:39:34 -0500

    Manifest and server workers are generated on build.  Need to setup service worker cacheing strategies.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,34 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'J.A.T.E'
-      }),      
+      }),
+
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'J.A.T.E Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Text editor progressive web application featuring a number of persistent data techniques.',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
@@ -34,17 +61,16 @@ module.exports = () => {
           use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
           test: /\.m?js$/,
           exclude: /node_modules/,
           // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-transform-async-to-generator'
+              ],
             },
           },
         },
